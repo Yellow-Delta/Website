da913cd5baadb6cc3921b7f77bdc0e08d56940ad
Supermuctec <supermutec@gmail.com>
2025-10-19 09:56:15 +0530

    Added security to admin api

--- Api/admin.py
+++ Api/admin.py
@@ -1,22 +1,30 @@
 from flask import *
 import sys
 import os
+import dotenv
+import bcrypt
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from functions import *
-
+ 
 admin = Blueprint("admin", __name__)
+config = dotenv.dotenv_values(".env")
 
+admin_key=config["ADMIN_KEY"]
+admin_key = bcrypt.hashpw(admin_key.encode("UTF-8"), bcrypt.gensalt())
 
 # Format: {title, description}
 @admin.route("/api/admin/project/add", methods=["POST"])
 def add_project():
     try:
-        title = request.form.get("title")
-        description = request.form.get("description")
-        image = request.form.get("image")
-        link = request.form.get("link")
-        slug = request.form.get("slug")
+        input_key=request.form.get("admin_key")
+        if bcrypt.checkpw(input_key.encode("UTF-8"), admin_key):
+            title = request.form.get("title")
+            description = request.form.get("description")
+            image = request.form.get("image")
+            link = request.form.get("link")
+            slug = request.form.get("slug")
+        else:
+            return jsonify(error="403 Unauthorized", success=False)
 
         return jsonify(error=None, success=True)
     except Exception as e:
